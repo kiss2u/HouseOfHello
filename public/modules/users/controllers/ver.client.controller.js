75248f2a0b9e47558dee521752bf0779876a17d7
Joe Zhou <joe.lamplight@gmail.com>
2015-02-24 19:25:54 -0800

    first changes

--- public/modules/users/controllers/ver.client.controller.js
+++ public/modules/users/controllers/ver.client.controller.js
@@ -4,25 +4,7 @@
 angular.module('verifications').controller('veriCtrl', ['$scope', '$stateParams', 'Verifications',
 	function($scope, $stateParams, Verifications) {
 		// Create new Verification
-		$scope.create = function() {
-			// Create new Verification object
-			var veri = new Verifications ({
-				veriNo: this.veriNo,
-				material: this.material,
-				style: this.style,
-				size: this.size,
-				veins: this.veins,
-				color: this.color,
-				agentID: this.agentID
 
-			});
-
-		// Find existing Verification
-		$scope.findOne = function() {
-			$scope.veri = Verifications.get({ 
-				veriNo: $stateParams.veriNo
-			});
-		};
 
 	}
 ]);
